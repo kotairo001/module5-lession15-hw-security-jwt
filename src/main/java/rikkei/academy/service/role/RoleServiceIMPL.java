7b1a373fe3540d3abab0f40bff2a83c163369333
vangtt <giangthanhvan12@gmail.com>
2023-06-09 08:21:02 +0700

    security

---
+++ src/main/java/rikkei/academy/service/role/RoleServiceIMPL.java
@@ -0,0 +1,19 @@
+package rikkei.academy.service.role;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import rikkei.academy.model.Role;
+import rikkei.academy.model.RoleName;
+import rikkei.academy.repository.IRoleRepository;
+
+import java.util.Optional;
+@Service
+public class RoleServiceIMPL implements IRoleService{
+    @Autowired
+    IRoleRepository repository;
+    @Override
+    public Optional<Role> findByName(RoleName name) {
+        return repository.findByName(name);
+    }
+}
+
